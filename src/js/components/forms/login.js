a88414e8e41f5f918d93e7d091855a926480dfec
AB-Macbook <aberry.consultancy@gmail.com>
2024-03-17 06:45:07 +1300

    #DEV - Fixed form label

--- src/js/components/forms/login.js
+++ src/js/components/forms/login.js
@@ -4,12 +4,14 @@ export default function (data) {
     loading: false,
     fields: [
       {
+        name: 'Username',
         type: 'input',
         placeholder: 'Username',
         label: 'Username',
         autocomplete: null
       },
       {
+        name: 'Password',
         type: 'password',
         placeholder: 'Password',
         label: 'Password',
@@ -33,15 +35,25 @@ export default function (data) {
       this.$root.innerHTML = `
       <form>
         <fieldset>
-          <template x-for="field in fields"> 
+          <template x-for="(field, i) in fields"> 
             <label>
-              <template x-text="field.name"></template>
+             <span x-text="field.label"></span>
               <input
                 :type="field.type"
                 :name="field.name"
+                :disabled="field.disabled"
+                :aria-label="field.ariaLabel || field.label"
+                :value="field.value"
+                x-model="field.value"
+                :read-only="field.readonly"
+                :role="field.role"
+                :checked="field.checked"
                 :placeholder="field.placeholder"
                 :autocomplete="field.autocomplete"
               />
+              <template x-if="field.helper != null">
+                <small :id="field.id || i" x-text="field.helper"></small>
+              </template>
             </label>
           </template>
         </fieldset>
