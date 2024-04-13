8fdafbf673ac70cb204306352568d7a6fa3aa099
AB-Macbook <aberry.consultancy@gmail.com>
2024-04-14 08:43:03 +1200

    #DEV - Updates to the form response component

--- src/js/components/app/appFormResponse.js
+++ src/js/components/app/appFormResponse.js
@@ -9,6 +9,9 @@ export default function (data) {
     tagStr: null,
     tags: [],
     showTags: false,
+    showText: false,
+    showVideo: false,
+    showImage: false,
     // INIT
     init() {
       this.tags = [];
@@ -19,6 +22,9 @@ export default function (data) {
       this.fields = data.fields,
       this.setHtml(data)
     },
+    // GETTERS
+    get typeSelected() { return this.showImage || this.showVideo || this.showText },
+    // METHODS
     async submit(fields) {
       this.loading = true;
       const payload = {}
@@ -45,6 +51,11 @@ export default function (data) {
     format(type) {
 
     },
+    cancelTypes() {
+      this.showText = false;
+      this.showVideo = false;
+      this.showImage = false;
+    },
     addTag() {
       this.tags.push(this.tagStr);
       this.tagStr = null;
@@ -55,35 +66,51 @@ export default function (data) {
       const html = `
         <div>
           <progress x-show="loading"></progress>
-          <fieldset x-data="formFields({fields})"></fieldset>
-          <footer align="right" style="text-align:right"> 
+          <fieldset role="search" x-data="formFields({fields})"></fieldset>
+         
+            <div >
+              <!-- Tags visible -->
+              <fieldset role="search" x-show="showTags">
+                <input name="Tag" type="text" x-model="tagStr" placeholder="tag your post.." />
+                <button class="small flat secondary material-icons" @click="addTag" :disabled="tagStr == null">add</button>
+                <button class="secondary flat material-icons" @click="showTags = !showTags"  :disabled="loading">chevron_right</button>
+                <button class="" @click="await submit(fields)" :disabled="loading">${label}</button>
+              </fieldset>
+            
+              <fieldset role="search" x-show="!showTags">
+                <!--Types-->
+                <button class="small secondary material-icons flat" x-show="!typeSelected" @click="showText = !showText" :disabled="loading">text_format</button>
+                <button class="small secondary material-icons flat" x-show="!typeSelected" @click="showVideo = !showVideo" :disabled="loading">videocam</button>
+                <button class="small secondary material-icons flat" x-show="!typeSelected" @click="showImage = !showImage" :disabled="loading">image</button>
+                <!--Cancel-->
+                <button class="small secondary material-icons flat" x-show="typeSelected" @click="cancelTypes" :disabled="loading">cancel</button>
 
-            <!-- Tags visible -->
-            <fieldset role="group" x-show="showTags">
-              <input name="Tag" type="text"  x-model="tagStr" placeholder="#tags" />
-              <button class="small flat secondary material-icons" @click="addTag" :disabled="tagStr == null">add</button>
-              <button class="small secondary outline material-icons" @click="showTags = !showTags"  :disabled="loading">chevron_right</button>
-              <button class="small" @click="await submit(fields)" :disabled="loading">${label}</button>
-            </fieldset>
+                <!--Type formats-->
+                <button class="small secondary material-icons flat small" x-show="showText" @click="showTags = !showTags" :disabled="loading">format_list_bulleted</button>
+                <button class="small secondary material-icons flat small" x-show="showText" @click="showTags = !showTags" :disabled="loading">format_list_numbered</button>
+                <button class="small secondary material-icons flat small" x-show="showText" @click="showTags = !showTags" :disabled="loading">link</button>
+                <button class="small secondary material-icons flat small" x-show="showText" @click="showTags = !showTags" :disabled="loading">format_quote</button>
+                <button class="small secondary material-icons flat small" x-show="showText" @click="showTags = !showTags" :disabled="loading">code</button>
+                
+                <input name="Tag" disabled type="text" placeholder="" />
+                
+                <button class=" secondary material-icons  flat" @click="showTags = !showTags" :disabled="loading">sell</button>
+                
+                <button class="" @click="await submit(fields)"  :disabled="loading">${label}</button>
 
-            <!-- Tags hidden -->
-            <fieldset role="group"  x-show="!showTags">
-              <input name="Tag" disabled type="text" placeholder="" />
-              
-              <button class="small secondary material-icons outline" @click="showTags = !showTags" :disabled="loading">chevron_left</button>
-              <button class="small" @click="await submit(fields)"  :disabled="loading">${label}</button>
-
-            </fieldset>
-
+              </fieldset>
+            </ul>
+         
+          <nav x-show="tags.length > 0" x-transition.scale.origin.top>
             <!-- Tags -->
-            <div class="grid" role="group" x-show="tags.length > 0" x-transition.scale.origin.top>
+            <div class="grid" role="group" >
               <div class="container">
                 <template x-for="(tag, i) in tags">
-                  <button class="tag closable flat secondary small" x-text="tag" @click="tags.splice(i, 1)"></button>
+                  <button class="tag closable outline secondary small" x-text="tag" @click="tags.splice(i, 1)"></button>
                 </template>
               </div>
             </div>
-          </footer>
+          </nav>
         </div>
       `
       this.$nextTick(() => {
